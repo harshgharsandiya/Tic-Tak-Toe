5b5c6deb6acf17af6aa01d6504a116c8a3fb04db
DevItaliya22 <132957942+DevItaliya22@users.noreply.github.com>
2023-05-28 16:44:08 +0530

    Update TicTacToeMain.java
    simpler and smaller

--- TicTacToeMain.java
+++ TicTacToeMain.java
@@ -1,6 +1,6 @@
 import java.util.*;
-public class TicTacToeMain{
-
+public class copystack
+{
     public static void print(char mat[][]){
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
@@ -8,126 +8,83 @@ public class TicTacToeMain{
             }
             System.out.println("\n");
         }
-        
+
     }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        char mat[][] = new char[3][3];
+    public static boolean rowwin(char mat[][])
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(mat[j][0]==mat[j][1] && mat[j][1]==mat[j][2] && mat[j][0]!=' ')
+                return true;
+        }
+        return false;
+
+    }
+    public static boolean colwin(char mat[][])
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(mat[0][j]==mat[1][j] && mat[1][j]==mat[2][j] && mat[0][j]!=' ')
+                return true;
+        }
+        return false;
+
+    }
+    public static boolean diagonalwin(char mat[][])
+    {
+
+            if(mat[0][0]==mat[1][1] && mat[1][1]==mat[2][2] && mat[0][0]!=' ')
+                return true;
+            else if(mat[0][2]==mat[1][1] && mat[1][1]==mat[2][0] && mat[1][1]!=' ')
+                return true;
+
+        return false;
+
+    }
+    public static void giveValue(char mat[][])
+    {
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
                 mat[i][j]=' ';
             }
         }
+    }
 
-        print(mat);
-        
+
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        char mat[][] = new char[3][3];
+        int row,col,pos;
         char ch;
-        int pos;
+        boolean flag=false;
 
-        outerloop:
+        giveValue(mat);
+
         for(int i=1; i<=9; i++){
-            pos=sc.nextInt(); // 1,2,3,4,5,6,7,8,9
-            
-            if(i%2 != 0){
-                ch= 'O';
-               
-                if(pos==1){
-                    mat[0][0]=ch;
-                }
-                else if(pos==2){
-                    mat[0][1]=ch;
-                }
-                else if(pos==3){
-                    mat[0][2]=ch;
-                }
-                else if(pos==4){
-                    mat[1][0]=ch;
-                }
-                else if(pos==5){
-                    mat[1][1]=ch;
-                }
-                else if(pos==6){
-                    mat[1][2]=ch;
-                }
-                else if(pos==7){
-                    mat[2][0]=ch;
-                }
-                else if(pos==8){
-                    mat[2][1]=ch;
-                }
-                else if(pos==9){
-                    mat[2][2]=ch;
-                }
-            }
-            else{
-                ch='X';
+            pos=sc.nextInt();
 
-                if(pos==1){
-                    mat[0][0]=ch;
-                }
-                else if(pos==2){
-                    mat[0][1]=ch;
-                }
-                else if(pos==3){
-                    mat[0][2]=ch;
-                }
-                else if(pos==4){
-                    mat[1][0]=ch;
-                }
-                else if(pos==5){
-                    mat[1][1]=ch;
-                }
-                else if(pos==6){
-                    mat[1][2]=ch;
-                }
-                else if(pos==7){
-                    mat[2][0]=ch;
-                }
-                else if(pos==8){
-                    mat[2][1]=ch;
-                }
-                else if(pos==9){
-                    mat[2][2]=ch;
-                }
-            }
+            if(i%2 != 0) ch = 'O';
+            else ch = 'X';
+
+              row =(pos-1)/3;
+            if(pos%3!=0)
+                 col=(pos%3)-1;
+            else
+                 col=2;
+
+            mat[row][col]=ch;
             print(mat);
 
-            for(int j=0; j<3; j++){
-                int k=0;
-                if(mat[j][k]==mat[j][k+1]){
-                    if(mat[j][k]==mat[j][k+2]){
-                        if(mat[j][k]!=' '){
-                            System.out.println(mat[j][k]+" is win");
-                            break outerloop;}
-                    }
-                }
-                    else if(mat[k][j]==mat[k+1][j]){
-                        if(mat[k][j]==mat[k+2][j]){
-                            if(mat[k][j]!=' '){
-                                System.out.println(mat[k][j]+" is win");
-                                break outerloop;
-                            }
-                         }
-                     }
-
-                     else if(mat[0][0]==mat[1][1]){
-                        if(mat[0][0]==mat[2][2]){
-                            if(mat[0][0]!=' '){
-                                System.out.println(mat[0][0]+" is win");
-                                break outerloop;
-                            }
-                         }
-                     }
-
-                     else if(mat[0][2]==mat[1][1]){
-                        if(mat[0][2]==mat[2][0]){
-                            if(mat[0][2]!=' '){
-                                System.out.println(mat[0][2]+" is win");
-                                break outerloop;
-                            }
-                         }
-                     }
+            if(rowwin(mat) || colwin(mat) || diagonalwin(mat))
+            {
+                flag=true;
+                System.out.println(mat[row][col]+"is the winner");
+                break;
             }
         }
+        if(!flag)
+            System.out.println("draw");
+        
     }
 }
